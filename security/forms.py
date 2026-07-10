7a68ec2b2f15e511ebbfce3e8023832c1a91b4f3
hahaewan11 <marloncalimpusan24@gmail.com>
2026-07-10 13:47:04 +0800

    Updated login page and forms

--- security/forms.py
+++ security/forms.py
@@ -105,18 +105,16 @@ class GBVReportForm(forms.Form):
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        label='Username',
-        max_length=100,
         widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter your username'
+            'class':'form-control',
+            'placeholder':'Username'
         })
     )
+
     password = forms.CharField(
-        label='Password',
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter your password'
+            'class':'form-control',
+            'placeholder':'Password'
         })
     )
     captcha = ReCaptchaField(
